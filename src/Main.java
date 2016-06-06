2c8db40da3dadb350c5a9efd865045c468ee0697
Aiko <akloster@thoughtworks.com>
2016-06-06 09:45:32 +0200

    add first 2 exercises

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * Created by akloster on 6/3/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Easiest exercise ever");
+        printAsterisk();
+        System.out.println("Draw a horizontal line");
+        printAsterisks2(8);
+    }
+
+    private static void printAsterisk() {
+        System.out.println("*");
+    }
+    private static void printAsterisks2(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+    }
+}
